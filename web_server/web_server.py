c880735934619933e6b8e79947fd9a3056b33fec
Osama Salahledin <osama@mx-os>
2024-10-25 08:44:59 +0400

    Fixed a bug for products with no local-market reviews but have global reviews

--- web_server/web_server.py
+++ web_server/web_server.py
@@ -93,4 +93,4 @@ def demand_prediction():
     return jsonify(result)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001, debug=True)
+    app.run(host='0.0.0.0', port=5001, debug=True, use_reloader=False)
